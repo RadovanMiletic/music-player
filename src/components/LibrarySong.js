c69df7f08e4d8c938f83c03ab90181a73c0a4c89
Radovan Miletic <radovangit@gmail.com>
2021-12-04 01:31:38 +0100

    deploy my music player app

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const LibrarySong = ({
+  song,
+  id,
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = async () => {
+    // const selectedSong = songs.filter((state) => state.id === id);
+    // setCurrentSong(selectedSong[0]);
+    await setCurrentSong(song);
+    //Add Active State
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    //check if the song is playing
+    if (isPlaying) audioRef.current.play();
+  };
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img alt={song.name} src={song.cover} />
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
